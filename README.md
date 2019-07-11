f59565d8d1ac2160a2b9f274c6ae9595384841ab
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-07-11 11:47:33 +0300

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+## Multilayer Perceptron Attention
+![](https://img.shields.io/badge/Python-2.7-brightgreen.svg)
+![](https://img.shields.io/badge/TensorFlow-1.4.1-yellowgreen.svg)
+
+![alt text](images/attention.png)
+
+This project is an unofficial implementation of multilayer perceptron 
+attenction network [[parper](https://www.aclweb.org/anthology/C16-1238)].
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from attention import AttentionYatianColing2016
 from config import AttentionYatianColing2016Config
 
 
-def initialize_session():
+def init_session():
     init_op = tf.global_variables_initializer()
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
@@ -19,49 +19,51 @@ def feed(attention):
     }
 
 
-feed_dict = {
-    "x": [
-        [1, 0, 1, 4, 2],
-        [1, 2, 0, 3, 0],
-        [1, 2, 1, 3, 3]
-    ],
+if __name__ == "__main__":
 
-    "entities": [
-        [0, 4],
-        [3, 1],
-        [3, 1]
-    ],
-}
+    feed_dict = {
+        "x": [
+            [1, 0, 1, 4, 2],
+            [1, 2, 0, 3, 0],
+            [1, 2, 1, 3, 3]
+        ],
 
-term_embedding = [
-        [1.0, 5.0, 1.0, 3.0, 0.0],
-        [2.0, 0.0, 2.0, 2.0, 1.0],
-        [3.0, 3.0, 1.0, 3.0, 3.0],
-        [4.0, 2.0, 4.0, 2.0, 4.0],
-        [5.0, 6.0, 1.0, 5.0, 0.0]
-    ]
+        "entities": [
+            [0, 4],
+            [3, 1],
+            [3, 1]
+        ],
+    }
 
-term_embedding_tensor = tf.constant(
-    value=term_embedding,
-    dtype=tf.float32,
-    shape=[len(term_embedding), len(term_embedding[0])])
+    term_embedding = [
+            [1.0, 5.0, 1.0, 3.0, 0.0],
+            [2.0, 0.0, 2.0, 2.0, 1.0],
+            [3.0, 3.0, 1.0, 3.0, 3.0],
+            [4.0, 2.0, 4.0, 2.0, 4.0],
+            [5.0, 6.0, 1.0, 5.0, 0.0]
+        ]
 
-attention = AttentionYatianColing2016(cfg=AttentionYatianColing2016Config(),
-                                      batch_size=len(feed_dict['x']),
-                                      terms_per_context=len(feed_dict['x'][0]),
-                                      term_embedding_size=len(term_embedding))
+    term_embedding_tensor = tf.constant(
+        value=term_embedding,
+        dtype=tf.float32,
+        shape=[len(term_embedding), len(term_embedding[0])])
 
-attention.init_input()
-attention.init_hidden()
-e_sum, weights = attention.init_body(term_embedding=term_embedding_tensor)
+    attention = AttentionYatianColing2016(cfg=AttentionYatianColing2016Config(),
+                                          batch_size=len(feed_dict['x']),
+                                          terms_per_context=len(feed_dict['x'][0]),
+                                          term_embedding_size=len(term_embedding))
 
-with initialize_session() as sess:
-    r_sum, r_weights = sess.run([e_sum, weights], feed_dict=feed(attention))
+    attention.init_input()
+    attention.init_hidden()
+    e_sum, weights = attention.init_body(term_embedding=term_embedding_tensor)
 
-    print "att_sum:"
-    print r_sum[0].shape
-    print r_sum[0]
+    with init_session() as sess:
+        r_sum, r_weights = sess.run([e_sum, weights], feed_dict=feed(attention))
 
-    print "att_weights:"
-    print r_weights[0].shape
-    print r_weights[0]
+        print "att_sum:"
+        print r_sum[0].shape
+        print r_sum[0]
+
+        print "att_weights:"
+        print r_weights[0].shape
+        print r_weights[0]
